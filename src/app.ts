591824927bea2020f5862b595cfefc483a258bee
lappq <lappq@skylab.vn>
2024-03-02 14:57:53 +0700

    add dotenv + try to test env

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import serverless from 'serverless-http';
 import express from 'express';
+import { config } from 'dotenv';
 
+config();
 
 const app = express();
 
 app.get('/', (req, res, next) => {
   return res.status(200).json({
-    message: 'Hello from root 333!',
+    message: 'Hello from root 333! ' + process.env.DB_URL,
   });
 });
 
